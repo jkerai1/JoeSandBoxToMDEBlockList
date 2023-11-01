2f5ed9102de93f4c081b481ea0af938d77ed0ed3
J/ <55988027+jkerai1@users.noreply.github.com>
2023-11-01 19:13:44 +0000

    Update JSB.py

--- JSB.py
+++ JSB.py
@@ -20,16 +20,14 @@ IOC_Columns = ["IndicatorType","IndicatorValue","ExpirationTime","Action","Sever
 stamp = datetime.datetime.now().strftime("%x").replace("/","-")
 filename = "joesandboxiocs+" + stamp + ".csv"
 fopen = Path(filename)
-
 if os.path.exists(filename)== False:
     with open(filename, 'a+',newline='') as file:
         writer = csv.writer(file)
         writer.writerow(IOC_Columns)
 
-##Query user
 query = input("what is the string to search in JSB? ")
 hashlist = []
-y = joe.analysis_search(query)#query to search
+y = joe.analysis_search(query)
 
 #print(json.dumps(y, sort_keys=True, indent=4, separators=(',', ':'))) use this for analyzing output
 
@@ -38,7 +36,7 @@ for z in y:
     if z['detection'] == "malicious" or z['detection'] == "suspicious":
         domain = tldextract.extract(z["filename"])
         if domain.suffix != "":
-            domainTLD= domain.domain + "." + domain.suffix #Block at highest level where possible
+            domainTLD= domain.domain + "." + domain.suffix #Block at highest level where possible,modify as required.
             if domainTLD not in domainlist and domainTLD not in whitelist:
                 domainlist.append(domainTLD)
 
@@ -50,11 +48,11 @@ for z in y:
 ##Write To File
 with open(filename, 'a',newline='') as file:
     writer = csv.writer(file)
-    for i in domainlist:#DomainName
-        writer.writerow(["DomainName",i,"","Block","","IOC","IOC","","","","","FALSE"])#Create MDE BlockList
+    for i in domainlist:
+        writer.writerow(["DomainName",i,"","Block","","JSB IOC","JSB IOC","","","","","FALSE"])
     for i in hashlist: 
-        writer.writerow(["FileSha256",i,"","Block","","IOC","IOC","","","","","FALSE"])
-        print("https://www.virustotal.com/gui/file/"+i)#verify hash result 
+        writer.writerow(["FileSha256",i,"","Block","","JSB IOC","JSB IOC","","","","","FALSE"])
+        print("https://www.virustotal.com/gui/file/"+i) #verify hash result 
         
-print(domainlist) #print URL list
+print(domainlist) 
 
