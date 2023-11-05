ff3a09536acad5f7f1925c1063659eed348a84de
J/ <55988027+jkerai1@users.noreply.github.com>
2023-11-05 15:33:55 +0000

    Create URLToTenantAllowBlockList.ps1

---
+++ URLToTenantAllowBlockList.ps1
@@ -0,0 +1,11 @@
+#Connect Exchange Powershell First https://learn.microsoft.com/en-us/powershell/exchange/connect-to-exchange-online-powershell?view=exchange-ps
+
+$csv = Import-CSV 'C:\Users\YOURPATH\joesandboxiocs+11-03-23.csv'
+
+foreach($line in $csv){
+    if ($line.IndicatorType -eq "DomainName"){
+        $url = $line.IndicatorValue + "/*"
+        #$url.insert(0,"~") #adjust wildcards accordingly, see https://learn.microsoft.com/en-us/microsoft-365/security/office-365-security/tenant-allow-block-list-urls-configure?view=o365-worldwide#url-entry-scenarios
+        New-TenantAllowBlockListItems -ListType Url -Block -Entries $url -NoExpiration -Notes "DnsTwist URL Block" 
+        }
+}
