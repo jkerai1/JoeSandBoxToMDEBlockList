ae0db9098a1bc599a7a999d02b4e0e3cc7233e4c
J/ <55988027+jkerai1@users.noreply.github.com>
2023-11-05 21:01:42 +0000

    Update URLToTenantAllowBlockList.ps1

--- URLToTenantAllowBlockList.ps1
+++ URLToTenantAllowBlockList.ps1
@@ -6,6 +6,6 @@ foreach($line in $csv){
     if ($line.IndicatorType -eq "DomainName"){
         $url = $line.IndicatorValue + "/*"
         #$url.insert(0,"~") #adjust wildcards accordingly, see https://learn.microsoft.com/en-us/microsoft-365/security/office-365-security/tenant-allow-block-list-urls-configure?view=o365-worldwide#url-entry-scenarios
-        New-TenantAllowBlockListItems -ListType Url -Block -Entries $url -NoExpiration -Notes "DnsTwist URL Block" 
+        New-TenantAllowBlockListItems -ListType Url -Block -Entries $url -NoExpiration -Notes $line.Description
         }
 }
