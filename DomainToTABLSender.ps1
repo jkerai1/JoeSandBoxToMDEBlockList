ce4b651d6da867dbe76ee324a131fd9e7c3b376e
J/ <55988027+jkerai1@users.noreply.github.com>
2023-11-05 15:08:44 +0000

    Create DomainToTABLSender.ps1

---
+++ DomainToTABLSender.ps1
@@ -0,0 +1,10 @@
+#Connect Exchange Powershell First https://learn.microsoft.com/en-us/powershell/exchange/connect-to-exchange-online-powershell?view=exchange-ps
+
+$csv = Import-CSV 'YOUR PATH/joesandboxiocs+11-03-23.csv'
+
+foreach($line in $csv){
+    if ($line.IndicatorType -eq "DomainName"){
+        $url = $line.IndicatorValue 
+        New-TenantAllowBlockListItems -ListType Sender -Block -Entries $url -NoExpiration
+        }
+}
