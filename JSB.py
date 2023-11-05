32a6cf91cf26b64e2c15170c9faf400ec3db6d88
J/ <55988027+jkerai1@users.noreply.github.com>
2023-11-05 11:11:58 +0000

    Update JSB.py

--- JSB.py
+++ JSB.py
@@ -1,39 +1,37 @@
 import csv
 import os
 import json
-import jbxapi
+import jbxapi #pip install required lib
 import tldextract
 import datetime
 from pathlib import Path
-domainlist = []
 from dotenv import load_dotenv
 
+domainlist = []
+hashlist = []
 whitelist = ["google.com","bing.com","microsoft.com","sendgrid.net","adobe.com","google.co.uk","sharepoint.com","rs6.net","mimecast.com","canva.com","targetx.com","canon-europe.com","ipfs.io","scc.com","outlook.com"]
 
-##Grab API Key######
-load_dotenv("jsb.env")
+load_dotenv("jsb.env") #Grab API Key from env
 key = os.getenv("API_KEY")
 joe = jbxapi.JoeSandbox(apikey=key,accept_tac=True)
 
-##BUILD CSV FILE#####
+#Build CSV File with column schema if file doesn't exist with timestamp, remove timestamp if not req'd for use case
 IOC_Columns = ["IndicatorType","IndicatorValue","ExpirationTime","Action","Severity","Title","Description","RecommendedActions","RbacGroups","Category","MitreTechniques","GenerateAlert"]
 stamp = datetime.datetime.now().strftime("%x").replace("/","-")
 filename = "joesandboxiocs+" + stamp + ".csv"
-fopen = Path(filename)
+
 if os.path.exists(filename)== False:
     with open(filename, 'a+',newline='') as file:
         writer = csv.writer(file)
         writer.writerow(IOC_Columns)
 
-query = input("what is the string to search in JSB? ")
+query = input("what is the string to search in JSB? ") #User Input could be removed and terms could be hardcoded
 if query == "":
-    query = "phish" 
-hashlist = []
-y = joe.analysis_search(query)
+    query = "phish" #Fallback
+    
+y = joe.analysis_search(query) #print(json.dumps(y, sort_keys=True, indent=4, separators=(',', ':'))) #use this for analyzing output
 
-#print(json.dumps(y, sort_keys=True, indent=4, separators=(',', ':'))) #use this for analyzing output
-
-##Grab URLs
+#Grab IOCS, first URLs then SHA256
 for z in y:
     if z['detection'] == "malicious" or z['detection'] == "suspicious":
         domain = tldextract.extract(z["filename"])
@@ -41,20 +39,14 @@ for z in y:
             domainAndTLD= domain.domain + "." + domain.suffix #Block at highest level where possible,modify as required.
             if domainAndTLD not in domainlist and domainAndTLD not in whitelist:
                 domainlist.append(domainAndTLD)
-
-##Grab SHA256
 for z in y:
     if (z['detection'] == "malicious" or z['detection'] == "suspicious") and z['sha256'] != "":
-        hashlist.append(z['sha256'])    
+        hashlist.append(z['sha256'])   
 
-##Write To File
 with open(filename, 'a',newline='') as file:
-    writer = csv.writer(file)
+    writer = csv.writer(file) #Write To File
     for i in domainlist:
-        writer.writerow(["DomainName",i,"","Block","","JSB IOC","JSB IOC","","","","","FALSE"])
+        writer.writerow(["DomainName",i,"","Block","","JSB IOC","JSB IOC","","","","","FALSE"]) #Fields are generic but could be customized with information from the API Request e.g analysis ID or external reference like VT
     for i in hashlist: 
         writer.writerow(["FileSha256",i,"","Block","","JSB IOC","JSB IOC","","","","","FALSE"])
-        print("https://www.virustotal.com/gui/file/"+i) #verify hash result 
-        
-print(domainlist) 
-
+        print("https://www.virustotal.com/gui/file/"+i) #verify hash result with external source
